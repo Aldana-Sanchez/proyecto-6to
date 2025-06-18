2ec04fee6c3da5d54de5a26770a74ee452d543a6
catalinavivanco <catalina.a.vivanco@gmail.com>
2025-06-17 23:41:40 -0300

    componente matematica

---
+++ src/components/materiaMatematica.js
@@ -0,0 +1,20 @@
+function MateriaMatematica() {
+  return (
+    <div className="pagina-materia">
+      <h2>Matemática</h2>
+      <div className="cuadro">
+        <h3>Programa</h3>
+        <p>Álgebra, funciones, trigonometría, análisis y geometría.</p>
+      </div>
+      <div className="cuadro">
+        <h3>Clases de consulta</h3>
+        <ul>
+          <li>Lunes 14:00 - 16:00</li>
+          <li>Miércoles 10:00 - 12:00</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default MateriaMatematica;
