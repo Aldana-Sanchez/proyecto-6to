81af61b12b2a97dc3e3303b1d1d0a8222f1ab30c
Aldana-Sanchez <sanchezaldana578@gmail.com>
2025-08-09 00:37:22 -0300

    Update App.js
    app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import './estilo.css';
-import FormularioInscripcion from './components/FormularioInscripcion';
-import BarraSuperior from './components/BarraSuperior';
+import React, { useState } from "react";
+import FormularioInscripcion from "./components/FormularioInscripcion";
+import MateriaMatematica from "./components/MateriaMatematica";
+
 
 function App() {
+  const [materiaVista, setMateriaVista] = useState("");
+
+  const seleccionarMateria = (materia) => {
+    setMateriaVista(materia);
+  };
+
+  const volverAlFormulario = () => {
+    setMateriaVista("");
+  };
+
   return (
-    
-    <div className="app">
-      <BarraSuperior />
-      <div className="contenido-principal">
-        <FormularioInscripcion />
-      </div>
-    </div>
+    <>
+      {materiaVista === "" && (
+        <FormularioInscripcion onMateriaSeleccionada={seleccionarMateria} />
+      )}
+      {materiaVista === "matematica" && (
+        <MateriaMatematica volver={volverAlFormulario} />
+      )}
+      
+    </>
   );
 }
 
