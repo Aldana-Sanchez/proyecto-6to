c749fddf2ebb6dbc3e08687ceb720034b4024db4
catalinavivanco <catalina.a.vivanco@gmail.com>
2025-08-22 19:12:02 -0300

    cambiioa

--- src/components/materiaMatematica.js
+++ src/components/materiaMatematica.js
@@ -1,18 +1,16 @@
+import React from "react";
+
 function MateriaMatematica() {
   return (
-    <div className="pagina-materia">
+    <div className="cuadro">
       <h2>Matemática</h2>
-      <div className="cuadro">
-        <h3>Programa</h3>
-        <p>Álgebra, funciones, trigonometría, análisis y geometría.</p>
-      </div>
-      <div className="cuadro">
-        <h3>Clases de consulta</h3>
-        <ul>
-          <li>Lunes 14:00 - 16:00</li>
-          <li>Miércoles 10:00 - 12:00</li>
-        </ul>
-      </div>
+      <h3>Programa</h3>
+      <p>Álgebra, funciones, trigonometría, análisis y geometría.</p>
+      <h3>Clases de consulta</h3>
+      <ul>
+        <li>Lunes 14:00 - 16:00</li>
+        <li>Miércoles 10:00 - 12:00</li>
+      </ul>
     </div>
   );
 }
