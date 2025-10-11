c2155493fb8c9f70ba880eecb8de380a939ada0a
M1lang4 <agustinafranciscavv@gmail.com>
2025-10-11 19:52:27 -0300

    Importaciones para Detalle.jsx .

---
+++ src/components/Detalle.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+
