972f63831a4ec18de06bff41e3a6c3979109f2d2
M1lang4 <agustinafranciscavv@gmail.com>
2025-10-11 21:52:46 -0300

    Creacion y modificacion AuthContext.js .

---
+++ src/components/AuthContext.js
@@ -0,0 +1,30 @@
+// src/context/AuthContext.js
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  const login = (userObj) => {
+    setUser(userObj);
+    navigate("/");
+  };
+
+  const logout = () => {
+    setUser(null);
+    navigate("/login");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
